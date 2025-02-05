e940450b7d59121f9afec2009a05ece31a669457
Muhammad Ali <2023mscs211@student.uet.edu.pk>
2025-02-05 17:10:27 +0500

    Create RomanUrduTokenizerUsingSpacy.py

---
+++ Tokenizer/RomanUrduTokenizerUsingSpacy.py
@@ -0,0 +1,31 @@
+import spacy
+
+class RomanUrduTokenizer:
+    def __init__(self):
+        # Load a blank spaCy model
+        self.nlp = spacy.blank('xx')  # 'xx' is for multi-language or custom pipelines
+
+    def tokenize(self, text):
+
+        # Process the text using spaCy
+        doc = self.nlp(text)
+        
+        # Extract tokens from the processed document
+        tokens = [token.text for token in doc]
+        return tokens
+
+# Example usage
+if __name__ == "__main__":
+    # Example Roman Urdu sentence
+    roman_urdu_sentence = "Yeh ek test sentence hai. Isme kuch special characters !@#$%^&*() hain."
+    paragraph = "kuch nahi hota..meri ma ka hai ausman card unko 2 baar hard atek ayaa tha ilaaj ke liy pase nahi the doctor ny bola is card se sarkar pase nahi milte"
+
+    # Create an instance of RomanUrduTokenizer
+    tokenizer = RomanUrduTokenizer()
+
+    # Tokenize the sentence
+    #tokens = tokenizer.tokenize(roman_urdu_sentence)
+    tokens = tokenizer.tokenize(paragraph)
+
+    # Print the tokens
+    print("Tokens:", tokens)
